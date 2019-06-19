2a140cf1a5922247483bc64c676655a461c2e46f
seckujdivad <juckes1david@gmail.com>
2019-06-19 14:53:07 +0100

    Set master form

--- Summer3D.dpr
+++ Summer3D.dpr
@@ -2,13 +2,19 @@ program Summer3D;
 
 uses
   Vcl.Forms,
-  FormRenderer in 'FormRenderer.pas' {Form1};
+  FormRenderer in 'FormRenderer.pas' {RenderForm},
+  URender in 'URender.pas',
+  UScene in 'UScene.pas',
+  FormDatabase in 'FormDatabase.pas' {DatabaseForm},
+  FormMain in 'FormMain.pas' {MainForm};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TRenderForm, RenderForm);
+  Application.CreateForm(TDatabaseForm, DatabaseForm);
+  Application.CreateForm(TMainForm, MainForm);
   Application.Run;
 end.
